7ee1a8d524763b89a5124a03290b03f0033611a7
bdklingerman <bdklingerman@gmail.com>
2026-09-20 07:17:15 -0500

    Import parameters from json.

--- src/adc_quantization/synthesize.py
+++ src/adc_quantization/synthesize.py
@@ -8,6 +8,8 @@ Version: September 18, 2026
 '''
 
 import argparse
+import json
+from scipy import signal
 import numpy
 import pandas
 
@@ -20,33 +22,44 @@ adc_bits = 12
 adc_span = 5
 fft_size = 8192
 
-def synthesize_signal(
+def synthesize_sine(
+        samples: numpy.ndarray,
         amplitude: float,
         freq: float,
         offset: float,
-        fs: float,
-        length: int
-    ) -> tuple[numpy.ndarray, numpy.ndarray]:
-    '''Generates t and v arrays based on input parameters.'''
+    ) -> numpy.ndarray:
+    '''Generates v array based on t and input parameters.'''
 
     # Handle exceptions
     if amplitude <= 0:
         raise ValueError(f"amplitude {amplitude} must be positive")
     if freq <= 0:
         raise ValueError(f"frequency {freq} must be positive")
-    if fs <= 0:
-        raise ValueError(f"sampling frequency {fs} must be positive")
-    if fs < 2 * freq:
-        raise ValueError(f"sampling frequency {fs} must be above Nyquist {2 * freq}")
-    if length <= 0:
-        raise ValueError(f"record length {length} must be positive")
 
     # Generate sine wave axes
-    t = numpy.arange(length) / fs
-    v = amplitude * numpy.sin(2 * numpy.pi * freq * t) + offset
+    return amplitude * numpy.sin(2 * numpy.pi * freq * samples) + offset
 
-    # Return as {t, v}
-    return t, v
+def synthesize_square(
+        samples: numpy.ndarray,
+        amplitude: float,
+        freq: float,
+        offset: float,
+        duty: float
+    ) -> numpy.ndarray:
+    '''Generates v array based on t and input parameters.'''
+
+    # Handle exceptions
+    if amplitude <= 0:
+        raise ValueError(f"amplitude {amplitude} must be positive")
+    if freq <= 0:
+        raise ValueError(f"frequency {freq} must be positive")
+    if duty < 0:
+        raise ValueError(f"duty cycle {duty} must be 0 or greater")
+    if duty > 1:
+        raise ValueError(f"duty cycle {duty} must be 1 or less")
+
+    # Generate square wave axis
+    return amplitude * signal.square(2 * numpy.pi * freq * samples, duty=duty) + offset
 
 def quantize_signal(signal: numpy.ndarray, bits: int, vref: float) -> numpy.ndarray:
     '''Simulate ADC quantization on a signal.'''
@@ -59,36 +72,40 @@ def quantize_signal(signal: numpy.ndarray, bits: int, vref: float) -> numpy.ndar
     return numpy.digitize(signal, adc)
 
 def main():
-    '''Parses input from CLI and saves it as csv and plot.'''
+    '''Defines signal from JSON and saves it as csv and plot.'''
 
-    # Parse arguments for waveform
+    # Parse arguments
     parser = argparse.ArgumentParser()
-    # TODO Add square wave
-    parser.add_argument("-n", "--name", type=str, default="raw_waveform")
-    parser.add_argument("-f", "--frequency", type=float, default=freq)
-    parser.add_argument("-a", "--amplitude", type =float, default=adc_span/2)
-    parser.add_argument("-o", "--offset", type=float, default=adc_span/2)
-    parser.add_argument("-s", "--sampling", type=float, default=fs)
-    parser.add_argument("-b", "--bits", type=int, default=adc_bits)
-    parser.add_argument("-m", "--length", type=int, default=fft_size)
-    parser.add_argument("-r", "--reference", type=float, default=adc_span)
+    parser.add_argument("-p", "--parameters", type=str, default="demo_parameters")
     args = parser.parse_args()
 
+    # Load parameters from JSON
+    with open(f"data/{args.parameters}.json") as file:
+        params = json.load(file)
+
+    # Define timebase
+    t = numpy.arange(params["length"]) / params["fs"]
+
     # Generate the waveform
-    t, v = synthesize_signal(amplitude=args.amplitude, freq=args.frequency, offset=args.offset, fs=args.sampling, length=args.length)
+    if (params["waveform"] == "sine"):
+        v = synthesize_sine(samples=t, amplitude=params["amplitude"], freq=params["frequency"], offset=params["offset"])
+    elif (params["waveform"] == "square"):
+        v = synthesize_sine(samples=t, amplitude=params["amplitude"], freq=params["frequency"], offset=params["offset"], duty=0.5)
+    else:
+        raise ValueError(f"invalid waveform {params["waveform"]}")
 
     # Plot the raw data
-    visualize.save_plot(y=v,x=t,filename="raw_waveform")
+    visualize.save_plot(y=v,x=t,filename=params["name"])
 
     # Quantize the signal
-    quantized_codes = quantize_signal(signal=v, bits=args.bits, vref=args.reference)
+    quantized_codes = quantize_signal(signal=v, bits=params["bits"], vref=params["vref"])
 
     # Plot the quantized data
     visualize.save_plot(y=quantized_codes,x=t,filename="adc_waveform")
 
     # Save the data
     frame = pandas.DataFrame({"Time (s)": t, "Voltage (v)": v, "Codes": quantized_codes})
-    frame.to_csv(f"data/{args.name}.csv", index=False)
+    frame.to_csv(f"data/{params["name"]}.csv", index=False)
 
 if __name__ == "__main__":
     main()
